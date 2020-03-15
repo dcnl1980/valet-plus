9d36db675ca68e64d5302a7542670eb8fde5d124
Christiaan van Steenbergen <cvsteenbergen@gmail.com>
2020-03-15 23:12:52 +0100

    Update CommandLine.php

--- cli/Valet/CommandLine.php
+++ cli/Valet/CommandLine.php
@@ -74,7 +74,7 @@ class CommandLine
     {
         $onError = $onError ?: function () {};
 
-        $process = new Process($command);
+        $process = new Process(array($command));
 
         $processOutput = '';
         $process->setTimeout(null)->run(function ($type, $line) use (&$processOutput) {
